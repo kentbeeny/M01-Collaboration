b28e3bb5dffcac6c106cdf9788502070bad859e3
kentbeeny <50148528+kentbeeny@users.noreply.github.com>
2023-11-01 21:36:50 -0500

    add helloUser.py

---
+++ helloUser.py
@@ -0,0 +1,23 @@
+'''
+helloUser.py
+Author: Kent Beeny
+Date: 11/1/2023
+version 1.0
+'''
+
+#initialization
+statement = 'Hello,'
+name = ''
+
+#get data
+name = input("Please enter your name: Ex 'Bob' ") #all inputs bring data in as a string
+#int
+#float
+#
+
+#process data
+statement = statement + ' ' + name + '.'
+
+#output information
+print(statement)
+#str
